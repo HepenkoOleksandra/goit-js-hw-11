67286450363c4ca19092a0f63ba15ca8345de8e4
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-01-31 00:02:18 +0200

    'update'

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+// import css from "file.css";
