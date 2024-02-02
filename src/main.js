409deffa731c6665ca6fd2116a5181c97da7d9f1
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-02-02 12:46:35 +0200

    'update'

--- src/main.js
+++ src/main.js
@@ -7,15 +7,19 @@ import "izitoast/dist/css/iziToast.min.css";
 const refs = {
     form: document.querySelector('.get-category'),
     input: document.querySelector('.query-category'),
+    galleryList: document.querySelector('.gallery')
 }
 
 refs.form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
+  
+    refs.galleryList.innerHTML = ''; // подумати
+       
     e.preventDefault();
-
+ 
     const query = e.target.elements.query.value.trim();
-
+    
 if (query === '') {
         alert('Please enter a category name'); // iziToast 
         return;
@@ -29,19 +33,21 @@ if (query === '') {
                 backgroundColor: '#e3f2ff',
                 position: 'topRight',
             });
+        } else {
+            renderGallery(data);
         }
-        //render
+        
     });
 
     e.target.reset();
 }
 
-function getGallery(q) {
+function getGallery(category) {
     const BASE_URL = 'https://pixabay.com';
     const END_POINT = '/api/';
     const PARAMS = new URLSearchParams({
         key: "42112521-3ff4dfc201bab0977369cd2bc",
-        q: `${q}`,
+        q: `${category}`,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
@@ -51,6 +57,31 @@ function getGallery(q) {
    return fetch(url).then(res => res.json());
    
 }
+
+
+function galleryTemplate(element) {
+    const {largeImageURL, webformatURL, tags, likes, views, comments, downloads} = element;
+    return `
+   <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+            <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+        </a>
+        <div class="gallery-body">
+            <p class="gallery-info">Likes: ${likes}</p>
+            <p class="gallery-info">Views: ${views}</p>
+            <p class="gallery-info">Comments: ${comments}</p>
+            <p class="gallery-info">Downloads: ${downloads}</p>
+        </div>
+    </li>`
+}
+
+function renderGallery(elements) {
+    const markup = elements.hits.map((element)=>{return galleryTemplate(element)})
+        .join('\n');
+       
+    refs.galleryList.insertAdjacentHTML('beforeend', markup);
+}
+
 
 
 
